1ac021a441b09fbf94e681a4c99a3564b4e543b5
redsteve <redsteve@gmx.de>
2011-06-13 19:43:05 +0200

    Form layout changed, check box for validation added.

--- EA_ReqIF_AddIn/ImportExportForm.Designer.cs
+++ EA_ReqIF_AddIn/ImportExportForm.Designer.cs
@@ -38,7 +38,8 @@ namespace EA_ReqIF_AddIn
 		{
 			this.components = new System.ComponentModel.Container();
 			System.Windows.Forms.Label label2;
-			System.Windows.Forms.GroupBox groupBox1;
+			System.Windows.Forms.GroupBox modeSelectionGroupBox;
+			this.validateXmlCheckBox = new System.Windows.Forms.CheckBox();
 			this.mergeRequirementsRadioButton = new System.Windows.Forms.RadioButton();
 			this.importRequirementsRadioButton = new System.Windows.Forms.RadioButton();
 			this.exportRequirementsRadioButton = new System.Windows.Forms.RadioButton();
@@ -50,41 +51,52 @@ namespace EA_ReqIF_AddIn
 			this.label1 = new System.Windows.Forms.Label();
 			this.selectedPackageTextBox = new System.Windows.Forms.TextBox();
 			label2 = new System.Windows.Forms.Label();
-			groupBox1 = new System.Windows.Forms.GroupBox();
-			groupBox1.SuspendLayout();
+			modeSelectionGroupBox = new System.Windows.Forms.GroupBox();
+			modeSelectionGroupBox.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pathFileTextBoxErrorProvider)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// label2
 			// 
 			label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			label2.Location = new System.Drawing.Point(12, 181);
+			label2.Location = new System.Drawing.Point(12, 214);
 			label2.Name = "label2";
 			label2.Size = new System.Drawing.Size(407, 16);
-			label2.TabIndex = 4;
+			label2.TabIndex = 3;
 			label2.Text = "Specify the path and the name of the ReqIF file:";
 			// 
-			// groupBox1
+			// modeSelectionGroupBox
 			// 
-			groupBox1.Controls.Add(this.mergeRequirementsRadioButton);
-			groupBox1.Controls.Add(this.importRequirementsRadioButton);
-			groupBox1.Controls.Add(this.exportRequirementsRadioButton);
-			groupBox1.Location = new System.Drawing.Point(12, 55);
-			groupBox1.Name = "groupBox1";
-			groupBox1.Size = new System.Drawing.Size(410, 123);
-			groupBox1.TabIndex = 8;
-			groupBox1.TabStop = false;
-			groupBox1.Text = "What would you like to do?";
+			modeSelectionGroupBox.Controls.Add(this.validateXmlCheckBox);
+			modeSelectionGroupBox.Controls.Add(this.mergeRequirementsRadioButton);
+			modeSelectionGroupBox.Controls.Add(this.importRequirementsRadioButton);
+			modeSelectionGroupBox.Controls.Add(this.exportRequirementsRadioButton);
+			modeSelectionGroupBox.Location = new System.Drawing.Point(12, 55);
+			modeSelectionGroupBox.Name = "modeSelectionGroupBox";
+			modeSelectionGroupBox.Size = new System.Drawing.Size(410, 145);
+			modeSelectionGroupBox.TabIndex = 2;
+			modeSelectionGroupBox.TabStop = false;
+			modeSelectionGroupBox.Text = "What would you like to do?";
+			// 
+			// validateXmlCheckBox
+			// 
+			this.validateXmlCheckBox.Location = new System.Drawing.Point(31, 47);
+			this.validateXmlCheckBox.Name = "validateXmlCheckBox";
+			this.validateXmlCheckBox.Size = new System.Drawing.Size(375, 24);
+			this.validateXmlCheckBox.TabIndex = 1;
+			this.validateXmlCheckBox.Text = "Validate the file on conformity with the ReqIF 1.0 specification (XSD).";
+			this.validateXmlCheckBox.UseVisualStyleBackColor = true;
 			// 
 			// mergeRequirementsRadioButton
 			// 
-			this.mergeRequirementsRadioButton.Location = new System.Drawing.Point(12, 51);
+			this.mergeRequirementsRadioButton.Location = new System.Drawing.Point(12, 73);
 			this.mergeRequirementsRadioButton.Name = "mergeRequirementsRadioButton";
 			this.mergeRequirementsRadioButton.Size = new System.Drawing.Size(394, 35);
 			this.mergeRequirementsRadioButton.TabIndex = 2;
 			this.mergeRequirementsRadioButton.Text = "Merge requirements from a ReqIF file and the current model (This mode provides fu" +
 			"nctionalities to solve conflicts).";
 			this.mergeRequirementsRadioButton.UseVisualStyleBackColor = true;
+			this.mergeRequirementsRadioButton.CheckedChanged += new System.EventHandler(this.OnRadioButtonsSelectionChanged);
 			// 
 			// importRequirementsRadioButton
 			// 
@@ -92,54 +104,56 @@ namespace EA_ReqIF_AddIn
 			this.importRequirementsRadioButton.Location = new System.Drawing.Point(12, 19);
 			this.importRequirementsRadioButton.Name = "importRequirementsRadioButton";
 			this.importRequirementsRadioButton.Size = new System.Drawing.Size(394, 26);
-			this.importRequirementsRadioButton.TabIndex = 1;
+			this.importRequirementsRadioButton.TabIndex = 0;
 			this.importRequirementsRadioButton.TabStop = true;
 			this.importRequirementsRadioButton.Text = "Initial import of requirements from a ReqIF file.";
 			this.importRequirementsRadioButton.UseVisualStyleBackColor = true;
+			this.importRequirementsRadioButton.CheckedChanged += new System.EventHandler(this.OnRadioButtonsSelectionChanged);
 			// 
 			// exportRequirementsRadioButton
 			// 
-			this.exportRequirementsRadioButton.Location = new System.Drawing.Point(12, 92);
+			this.exportRequirementsRadioButton.Location = new System.Drawing.Point(12, 110);
 			this.exportRequirementsRadioButton.Name = "exportRequirementsRadioButton";
 			this.exportRequirementsRadioButton.Size = new System.Drawing.Size(394, 24);
 			this.exportRequirementsRadioButton.TabIndex = 3;
 			this.exportRequirementsRadioButton.Text = "Export requirements from the model to a ReqIF file";
 			this.exportRequirementsRadioButton.UseVisualStyleBackColor = true;
+			this.exportRequirementsRadioButton.CheckedChanged += new System.EventHandler(this.OnRadioButtonsSelectionChanged);
 			// 
 			// pathFileTextBox
 			// 
-			this.pathFileTextBox.Location = new System.Drawing.Point(12, 200);
+			this.pathFileTextBox.Location = new System.Drawing.Point(12, 233);
 			this.pathFileTextBox.Name = "pathFileTextBox";
 			this.pathFileTextBox.Size = new System.Drawing.Size(345, 20);
-			this.pathFileTextBox.TabIndex = 5;
+			this.pathFileTextBox.TabIndex = 4;
 			this.pathFileTextBox.Validated += new System.EventHandler(this.OnValidatePathFileTextBox);
 			// 
 			// fileDialogButton
 			// 
-			this.fileDialogButton.Location = new System.Drawing.Point(378, 200);
+			this.fileDialogButton.Location = new System.Drawing.Point(378, 233);
 			this.fileDialogButton.Name = "fileDialogButton";
 			this.fileDialogButton.Size = new System.Drawing.Size(41, 21);
-			this.fileDialogButton.TabIndex = 6;
+			this.fileDialogButton.TabIndex = 5;
 			this.fileDialogButton.Text = "...";
 			this.fileDialogButton.UseVisualStyleBackColor = true;
 			this.fileDialogButton.Click += new System.EventHandler(this.OnFileDialogButtonClick);
 			// 
 			// startButton
 			// 
-			this.startButton.Location = new System.Drawing.Point(347, 237);
+			this.startButton.Location = new System.Drawing.Point(347, 280);
 			this.startButton.Name = "startButton";
 			this.startButton.Size = new System.Drawing.Size(75, 23);
-			this.startButton.TabIndex = 8;
+			this.startButton.TabIndex = 7;
 			this.startButton.Text = "Start";
 			this.startButton.UseVisualStyleBackColor = true;
 			this.startButton.Click += new System.EventHandler(this.OnStartButtonClick);
 			// 
 			// cancelButton
 			// 
-			this.cancelButton.Location = new System.Drawing.Point(266, 237);
+			this.cancelButton.Location = new System.Drawing.Point(266, 280);
 			this.cancelButton.Name = "cancelButton";
 			this.cancelButton.Size = new System.Drawing.Size(75, 23);
-			this.cancelButton.TabIndex = 7;
+			this.cancelButton.TabIndex = 6;
 			this.cancelButton.Text = "Cancel";
 			this.cancelButton.UseVisualStyleBackColor = true;
 			this.cancelButton.Click += new System.EventHandler(this.OnCancelButtonClick);
@@ -152,8 +166,8 @@ namespace EA_ReqIF_AddIn
 			// 
 			this.label1.Location = new System.Drawing.Point(12, 9);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(226, 17);
-			this.label1.TabIndex = 9;
+			this.label1.Size = new System.Drawing.Size(407, 17);
+			this.label1.TabIndex = 0;
 			this.label1.Text = "Current selected package in the model tree:";
 			// 
 			// selectedPackageTextBox
@@ -162,15 +176,15 @@ namespace EA_ReqIF_AddIn
 			this.selectedPackageTextBox.Name = "selectedPackageTextBox";
 			this.selectedPackageTextBox.ReadOnly = true;
 			this.selectedPackageTextBox.Size = new System.Drawing.Size(406, 20);
-			this.selectedPackageTextBox.TabIndex = 10;
+			this.selectedPackageTextBox.TabIndex = 1;
 			// 
 			// ImportExportForm
 			// 
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-			this.ClientSize = new System.Drawing.Size(434, 272);
+			this.ClientSize = new System.Drawing.Size(434, 315);
 			this.Controls.Add(this.selectedPackageTextBox);
 			this.Controls.Add(this.label1);
-			this.Controls.Add(groupBox1);
+			this.Controls.Add(modeSelectionGroupBox);
 			this.Controls.Add(this.cancelButton);
 			this.Controls.Add(this.startButton);
 			this.Controls.Add(this.fileDialogButton);
@@ -182,11 +196,12 @@ namespace EA_ReqIF_AddIn
 			this.Name = "ImportExportForm";
 			this.ShowInTaskbar = false;
 			this.Text = "ReqIF Import/Export Add-In";
-			groupBox1.ResumeLayout(false);
+			modeSelectionGroupBox.ResumeLayout(false);
 			((System.ComponentModel.ISupportInitialize)(this.pathFileTextBoxErrorProvider)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 		}
+		private System.Windows.Forms.CheckBox validateXmlCheckBox;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.TextBox selectedPackageTextBox;
 		private System.Windows.Forms.ErrorProvider pathFileTextBoxErrorProvider;
