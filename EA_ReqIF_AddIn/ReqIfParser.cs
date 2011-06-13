6e8135eafde8ad8a0d31980faefa3ca372e9ce7b
redsteve <redsteve@gmx.de>
2011-06-13 19:46:32 +0200

    Implementation of XML validation.

--- EA_ReqIF_AddIn/ReqIfParser.cs
+++ EA_ReqIF_AddIn/ReqIfParser.cs
@@ -1,59 +1,102 @@
-﻿/*
- * $Id:$
- */
-
-using System;
+﻿using System;
+using System.IO;
+using System.Collections;
 using System.Xml;
+using System.Xml.Schema;
 
 namespace EA_ReqIF_AddIn
 {
 	/// <summary>
-	/// This class represents the Requirements Interchange Format (ReqIF) Parser.
+	/// This class represents a validating Requirements Interchange Format (ReqIF) Parser.
 	/// </summary>
 	public sealed class ReqIfParser
 	{
 		private XmlReader xmlReader;
+		private static string xsdURI = "reqif.xsd";
+		private static ArrayList validationMessages = new ArrayList();
 		
 		/// <summary>
 		/// An initialization constructor.
 		/// </summary>
-		/// <param name="filename">A string containing the path and the name of the RIF file.</param>
-		public ReqIfParser(ref String filename)
+		/// <param name="filename">A string containing the path and the name of the ReqIF file.</param>
+		/// <param name="doValidate">Set this to <c>true</c> to validate the ReqIF file.</param>
+		public ReqIfParser(String filename, bool doValidate)
 		{
-			checkFilenameArgument(ref filename);
-			XmlReaderSettings settings = retrieveXmlReaderSettings();
-			
-			xmlReader = XmlReader.Create(filename, settings);
+			CheckFilenameArgument(filename);
+			xmlReader = XmlTextReader.Create(filename);
+			if (doValidate)
+			{
+				ValidateXmlDocument(filename);
+			}
 		}
-
-		public void parse(IReqIfParserCallbackReceiver callbackReceiver)
+		
+		public void Parse(IReqIfParserCallbackReceiver callbackReceiver)
 		{
 			if (xmlReader != null)
 			{
 				while (xmlReader.Read())
 				{
-					System.Console.WriteLine(xmlReader.Name);
+					string xmlString = xmlReader.ReadString();
 				}
 			}
 		}
+
+		public static ArrayList ValidationResultMessages
+		{
+			get { return validationMessages; }
+		}
 		
-		private void checkFilenameArgument(ref String filename)
+		private void CheckFilenameArgument(String filename)
 		{
 			if (filename == null) {
 				throw new System.ArgumentNullException("The filename must not be null!");
 			}
-			if (filename.Length == 0) {
+			if (filename == String.Empty) {
 				throw new System.ArgumentException("The filename must not be empty!");
 			}
 		}
 
-		private XmlReaderSettings retrieveXmlReaderSettings()
+		private void ValidateXmlDocument(String filename)
+		{
+			StreamReader xsdStreamReader = new StreamReader(xsdURI);
+			XmlSchema schema = new XmlSchema();
+			
+			schema = XmlSchema.Read(xsdStreamReader,
+			                        new ValidationEventHandler(XmlSchemaValidationCallBack));
+			XmlReaderSettings xmlReaderSettings = RetrieveXmlReaderSettings(schema);
+			XmlReader validationXmlReader = XmlReader.Create(xmlReader, xmlReaderSettings);
+
+			while (validationXmlReader.Read())
+			{ /* Empty loop */ }
+		}
+		
+		private XmlReaderSettings RetrieveXmlReaderSettings(XmlSchema schemaForValidation)
 		{
 			XmlReaderSettings settings = new XmlReaderSettings();
-			settings.ConformanceLevel = ConformanceLevel.Fragment;
+			settings.ValidationEventHandler += new ValidationEventHandler(XmlSchemaValidationCallBack);
+			settings.Schemas = new XmlSchemaSet();
+			
+			settings.ValidationType = ValidationType.Schema;
+			settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+			settings.Schemas.Add(schemaForValidation);
+			settings.ConformanceLevel = ConformanceLevel.Auto;
 			settings.IgnoreWhitespace = true;
 			settings.IgnoreComments = true;
+
 			return settings;
+		}
+
+		private XmlSchemaSet createXmlSchemaSetForValidation()
+		{
+			XmlSchemaSet xmlSchemaSet = new XmlSchemaSet();
+			xmlSchemaSet.XmlResolver = new XmlUrlResolver();
+			xmlSchemaSet.Add(null, xsdURI);
+			return xmlSchemaSet;
+		}
+
+		private static void XmlSchemaValidationCallBack(object sender, ValidationEventArgs args)
+		{
+			validationMessages.Add("[" + args.Exception.GetType().ToString() + "]: " + args.Message);
 		}
 	}
 }
