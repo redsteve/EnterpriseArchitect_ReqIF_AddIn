97ebc93d1231719e365ac3d64c7b760f730d10eb
redsteve <redsteve@gmx.de>
2011-06-20 05:52:33 +0200

    Implementing parts from IReqIfParserCallbackReceiver.
    This file file contains now implementations of methods declared in interface IReqIfParserCallbackReceiver.

--- EA_ReqIF_AddIn/RequirementsFromReqIfFileImporter.cs
+++ EA_ReqIF_AddIn/RequirementsFromReqIfFileImporter.cs
@@ -8,9 +8,107 @@ namespace EA_ReqIF_AddIn
 	/// </summary>
 	public class RequirementsFromReqIfFileImporter : IReqIfParserCallbackReceiver
 	{
+		private enum ProcessingStage
+		{
+			Start,
+			ProcessingReqIfDocument,
+			ProcessingReqIfHeader,
+		}
+		
+		private ProcessingStage stage;
+		
 		public RequirementsFromReqIfFileImporter()
 		{
-
+			stage = ProcessingStage.Start;
+		}
+		
+		public void ProcessElementStartNode(string name)
+		{
+			switch (name)
+			{
+				case "REQ-IF":
+					EnteringReqIfDocument();
+					break;
+					
+				case "REQ-IF-HEADER":
+					EnteringReqIfHeader();
+					break;
+			}
+		}
+		
+		public void ProcessTextNode(string name)
+		{
+			// throw new NotImplementedException();
+		}
+		
+		public void ProcessElementEndNode(string name)
+		{
+			switch (name)
+			{
+				case "REQ-IF-HEADER":
+					LeavingReqIfHeader();
+					break;
+					
+				case "REQ-IF":
+					LeavingReqIfDocument();
+					break;
+			}
+		}
+		
+		private void EnteringReqIfDocument()
+		{
+			switch (stage)
+			{
+				case ProcessingStage.Start:
+					stage = ProcessingStage.ProcessingReqIfDocument;
+					break;
+					
+				default:
+					throw new InvalidOperationException();
+					
+			}
+		}
+		
+		private void LeavingReqIfDocument()
+		{
+			switch (stage)
+			{
+				case ProcessingStage.Start:
+					stage = ProcessingStage.ProcessingReqIfDocument;
+					break;
+					
+				default:
+					throw new InvalidOperationException();
+					
+			}
+		}
+		
+		private void EnteringReqIfHeader()
+		{
+			switch (stage)
+			{
+				case ProcessingStage.ProcessingReqIfDocument:
+					stage = ProcessingStage.ProcessingReqIfHeader;
+					break;
+					
+				default:
+					throw new InvalidOperationException();
+					
+			}
+		}
+		
+		private void LeavingReqIfHeader()
+		{
+			switch (stage)
+			{
+				case ProcessingStage.ProcessingReqIfHeader:
+					stage = ProcessingStage.ProcessingReqIfDocument;
+					break;
+					
+//				default:
+//					throw new InvalidOperationException();
+					
+			}
 		}
 	}
 }
