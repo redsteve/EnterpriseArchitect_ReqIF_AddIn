33184afb03e2dcf2b7b1320cd20d32feba75d702
redsteve <redsteve@gmx.de>
2011-06-23 21:56:09 +0200

    A new base class for all ...Importer classes.

---
+++ EA_ReqIF_AddIn/BasicReqIfFileImporter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace EA_ReqIF_AddIn
+{
+	/// <summary>
+	/// Description of BasicReqIfFileImporter.
+	/// </summary>
+	public abstract class BasicReqIfFileImporter : IReqIfParserCallbackReceiver
+	{
+		protected IReqIfParserCallbackReceiver subImporter;
+		
+		protected BasicReqIfFileImporter()
+		{
+			subImporter = null;
+		}
+		
+		public abstract void ProcessElementStartNode(string name);
+		public abstract void ProcessAttribute(string name, string value);
+		public abstract void ProcessTextNode(string name);
+		public abstract void ProcessElementEndNode(string name);
+		
+		protected void PassElementStartNodeToSubImporter(string name)
+		{
+			if (subImporter != null)
+			{
+				subImporter.ProcessElementStartNode(name);
+			}
+		}
+
+		protected void PassAttributeToSubImporter(string name, string value)
+		{
+			if (subImporter != null)
+			{
+				subImporter.ProcessAttribute(name, value);
+			}
+		}
+
+		protected void PassTextNodeToSubImporter(string text)
+		{
+			if (subImporter != null)
+			{
+				subImporter.ProcessTextNode(text);
+			}
+		}
+		
+		protected void PassElementEndNodeToSubImporter(string name)
+		{
+			if (subImporter != null)
+			{
+				subImporter.ProcessElementEndNode(name);
+			}
+		}
+	}
+}
