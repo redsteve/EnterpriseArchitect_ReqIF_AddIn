e118323c6104f2623ebfe00296fc133d2716afda
redsteve <redsteve@gmx.de>
2011-06-26 16:43:35 +0200

    New qualified exception class for parser errors.

---
+++ EA_ReqIF_AddIn/ParserFailureException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EA_ReqIF_AddIn
+{
+	/// <summary>
+	/// The exception that is thrown when a parser error occurs.
+	/// </summary>
+	public class ParserFailureException : ApplicationException
+	{
+		public ParserFailureException() : base("An error has occured while parsing the file!")
+		{
+		}
+		
+		public ParserFailureException(string message) : base(message)
+		{
+		}
+		
+		public ParserFailureException(SerializationInfo info, StreamingContext context) :
+			base(info, context)
+		{
+		}
+		
+		public ParserFailureException(string message, Exception innerException) :
+			base(message, innerException)
+		{
+		}
+	}
+}
