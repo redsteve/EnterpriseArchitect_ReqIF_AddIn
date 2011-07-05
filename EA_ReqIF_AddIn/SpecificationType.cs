3daad4e0577874dd5499a136ba211e5757cb1804
redsteve <redsteve@gmx.de>
2011-07-05 22:09:19 +0200

    A very minor change in class documentation.

--- EA_ReqIF_AddIn/SpecificationType.cs
+++ EA_ReqIF_AddIn/SpecificationType.cs
@@ -3,7 +3,7 @@
 namespace EA_ReqIF_AddIn
 {
 	/// <summary>
-	/// This struct represents an ReqIF element <c>&lt;SPECIFICATION-TYPE&gt;</c>...<c>&lt;/SPECIFICATION-TYPE&gt;</c>.
+	/// This class represents an ReqIF element <c>&lt;SPECIFICATION-TYPE&gt;</c>...<c>&lt;/SPECIFICATION-TYPE&gt;</c>.
 	/// See OMG ReqIF specification chapter 10.8.43 for more information.
 	/// </summary>
 	public class SpecificationType : Identifiable
