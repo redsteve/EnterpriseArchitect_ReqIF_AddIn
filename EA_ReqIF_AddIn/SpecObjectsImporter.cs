27acd5aa4bc762eaa176c0686ebc3763d109c82d
redsteve <redsteve@gmx.de>
2011-07-05 22:11:21 +0200

    Continuation of implementation.

--- EA_ReqIF_AddIn/SpecObjectsImporter.cs
+++ EA_ReqIF_AddIn/SpecObjectsImporter.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using EA;
+using System.Collections;
 
 namespace EA_ReqIF_AddIn
 {
@@ -7,31 +7,108 @@ namespace EA_ReqIF_AddIn
 	/// Objects of this class are responsible for importing the parts of a ReqIF file between
 	/// XML nodes <c>&lt;SPEC-OBJECTS&gt;</c> and <c>&lt;/SPEC-OBJECTS&gt;</c>.
 	/// </summary>
-	public class SpecObjectsImporter : BasicReqIfFileImporter
+	public class SpecObjectsImporter : IdentifiablesImporter
 	{
-		public SpecObjectsImporter()
+		#region Constants
+		private const string specObjectNodeName = "SPEC-OBJECT";
+		private const string alternativeIdNodeName = "ALTERNATIVE-ID";
+		private const string valuesNodeName = "VALUES";
+		private const string typeNodeName = "TYPE";
+		private const string specObjectTypeRefNodeName = "SPEC-OBJECT-TYPE-REF";
+		#endregion
+		
+		private enum ProcessingElement
 		{
-
+			Undefined,
+			SpecObject,
+			AlternativeId,
+			Values,
+			Type,
+			SpecObjectTypeRef
+		}
+		
+		private ProcessingElement processingElement;
+		
+		public SpecObjectsImporter(ref Hashtable specificationObjects) : base(ref specificationObjects)
+		{
+			processingElement = ProcessingElement.Undefined;
 		}
 		
 		public override void ProcessTextNode(string text)
 		{
-			throw new NotImplementedException();
+			if (processingElement == ProcessingElement.SpecObjectTypeRef)
+			{
+				((SpecObject)identifiableElementUnderConstruction).SpecObjectTypeIdentifier = text;
+			} else {
+				throw new ParserFailureException(unexpectedTextNodeError);
+			}
 		}
 		
 		public override void ProcessElementStartNode(string name)
 		{
-			throw new NotImplementedException();
+			switch (name)
+			{	
+				case specObjectNodeName:
+					processingElement = ProcessingElement.SpecObject;
+					identifiableElementUnderConstruction = new SpecObject();
+					break;
+					
+				case alternativeIdNodeName:
+					if (processingElement == ProcessingElement.SpecObject)
+						processingElement = ProcessingElement.AlternativeId;
+					break;
+					
+				case valuesNodeName:
+					if (processingElement == ProcessingElement.SpecObject)
+						processingElement = ProcessingElement.Values;
+					break;
+					
+				case typeNodeName:
+					if (processingElement == ProcessingElement.SpecObject)
+						processingElement = ProcessingElement.Type;
+					break;
+					
+				case specObjectTypeRefNodeName:
+					if (processingElement == ProcessingElement.Type)
+						processingElement = ProcessingElement.SpecObjectTypeRef;
+					break;
+					
+				default:
+					throw new ParserFailureException(unexpectedElementNodeErrorText + name + "'.");
+			}
 		}
 		
 		public override void ProcessElementEndNode(string name)
 		{
-			throw new NotImplementedException();
-		}
-		
-		public override void ProcessAttribute(string name, string value)
-		{
-			throw new NotImplementedException();
+			switch (name)
+			{
+				case specObjectNodeName:
+					FinalizeIdentifiableElementUnderConstruction();
+					break;
+					
+				case alternativeIdNodeName:
+					if (processingElement == ProcessingElement.AlternativeId)
+						processingElement = ProcessingElement.SpecObject;
+					break;
+					
+				case valuesNodeName:
+					if (processingElement == ProcessingElement.Values)
+						processingElement = ProcessingElement.SpecObject;
+					break;
+					
+				case typeNodeName:
+					if (processingElement == ProcessingElement.Type)
+						processingElement = ProcessingElement.SpecObject;
+					break;
+					
+				case specObjectTypeRefNodeName:
+					if (processingElement == ProcessingElement.SpecObjectTypeRef)
+						processingElement = ProcessingElement.Type;
+					break;
+					
+				default:
+					throw new ParserFailureException(unexpectedElementNodeErrorText + name + "'.");
+			}
 		}
 	}
 }
