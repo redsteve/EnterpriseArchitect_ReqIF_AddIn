c55d062990f94c690b20e8ff2fa93ac4ddff551b
redsteve <redsteve@gmx.de>
2011-07-05 22:07:24 +0200

    New class.

---
+++ EA_ReqIF_AddIn/SpecObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EA_ReqIF_AddIn
+{
+	/// <summary>
+	/// This class represents an ReqIF element <c>&lt;SPEC-OBJECT&gt;</c>...<c>&lt;/SPEC-OBJECT&gt;</c>.
+	/// See OMG ReqIF specification chapter 10.8.43 for more information.
+	/// </summary>
+	public class SpecObject : Identifiable
+	{
+		private string alternativeId;
+		private string specObjectTypeIdentifier;
+		
+		public string AlternativeId {
+			get { return alternativeId; }
+			set { alternativeId = value; }
+		}
+		
+		public string SpecObjectTypeIdentifier {
+			get { return specObjectTypeIdentifier; }
+			set { specObjectTypeIdentifier = value; }
+		}
+		
+		public SpecObject()
+		{
+		}
+	}
+}
---
+++ EA_ReqIF_AddIn/SpecRelation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EA_ReqIF_AddIn
+{
+	/// <summary>
+	/// This class represents an ReqIF element <c>&lt;SPEC-RELATION&gt;</c>...<c>&lt;/SPEC-RELATION&gt;</c>.
+	/// See OMG ReqIF specification chapter 10.8.41 for more information.
+	/// </summary>
+	public class SpecRelation : Identifiable
+	{
+		private string specRelationTypeIdentifier;
+		private string sourceSpecObjectIdentifier;
+		private string targetSpecObjectIdentifier;
+		private string alternativeId;
+		
+		#region Properties
+		public string SpecRelationTypeIdentifier {
+			get { return specRelationTypeIdentifier; }
+			set { specRelationTypeIdentifier = value; }
+		}
+		
+		public string SourceSpecObjectIdentifier {
+			get { return sourceSpecObjectIdentifier; }
+			set { sourceSpecObjectIdentifier = value; }
+		}
+
+		public string TargetSpecObjectIdentifier {
+			get { return targetSpecObjectIdentifier; }
+			set { targetSpecObjectIdentifier = value; }
+		}
+		#endregion
+		
+		public SpecRelation()
+		{
+		}
+	}
+}
