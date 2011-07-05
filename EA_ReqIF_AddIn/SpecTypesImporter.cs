19470400f978d8a70622d051bd6ea40f93c09578
redsteve <redsteve@gmx.de>
2011-07-05 22:10:39 +0200

    New base class: Identifiable.
    New base class Identifiable makes it possible to remove tons of code here.

--- EA_ReqIF_AddIn/SpecTypesImporter.cs
+++ EA_ReqIF_AddIn/SpecTypesImporter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Diagnostics;
 using EA;
 
 namespace EA_ReqIF_AddIn
@@ -8,27 +9,22 @@ namespace EA_ReqIF_AddIn
 	/// Objects of this class are responsible for importing the parts of a ReqIF file between
 	/// XML nodes <c>&lt;SPEC-TYPES&gt;</c> and <c>&lt;/SPEC-TYPES&gt;</c>.
 	/// </summary>
-	public class SpecTypesImporter : BasicReqIfFileImporter
+	public class SpecTypesImporter : IdentifiablesImporter
 	{
 		#region Constants
 		private const string specificationTypeNodeName = "SPECIFICATION-TYPE";
 		private const string specObjectTypeNodeName = "SPEC-OBJECT-TYPE";
 		private const string specRelationTypeNodeName = "SPEC-RELATION-TYPE";
 		private const string relationGroupTypeNodeName = "RELATION-GROUP-TYPE";
-		private const string identifierAttributeName = "IDENTIFIER";
-		private const string descriptionAttributeName = "DESC";
-		private const string longNameAttributeName = "LONG-NAME";
-		private const string lastChangeAttributeName = "LAST-CHANGE";
 		#endregion
 		
-		private Hashtable specificationTypes;
-		private Identifiable identifiableElementUnderConstruction;
-		
-		public SpecTypesImporter(ref Hashtable specificationTypes)
+		/// <summary>
+		/// The initialization constructor.
+		/// <param name="specificationTypes">A reference to a hashtable to take up constructed specification types.</param>
+		/// </summary>
+		public SpecTypesImporter(ref Hashtable specificationTypes) : base(ref specificationTypes)
 		{
-			if (specificationTypes == null)
-				throw new ArgumentNullException("specificationTypes; Class: SpecTypesImporter");
-			this.specificationTypes = specificationTypes;
+
 		}
 		
 		public override void ProcessTextNode(string text)
@@ -41,23 +37,23 @@ namespace EA_ReqIF_AddIn
 			switch (name)
 			{
 				case specificationTypeNodeName:
-					identifiableElementUnderConstruction = (Identifiable)new SpecificationType();
+					identifiableElementUnderConstruction = new SpecificationType();
 					break;
 					
 				case specObjectTypeNodeName:
-					identifiableElementUnderConstruction = (Identifiable)new SpecObjectType();
+					identifiableElementUnderConstruction = new SpecObjectType();
 					break;
 					
 				case specRelationTypeNodeName:
-					identifiableElementUnderConstruction = (Identifiable)new SpecRelationType();
+					identifiableElementUnderConstruction = new SpecRelationType();
 					break;
 					
 				case relationGroupTypeNodeName:
-					identifiableElementUnderConstruction = (Identifiable)new RelationGroupType();
+					identifiableElementUnderConstruction = new RelationGroupType();
 					break;
 					
 				default:
-					throw new ParserFailureException(unexpectedElementNodeError + name + "'.");
+					throw new ParserFailureException(unexpectedElementNodeErrorText + name + "'.");
 			}
 		}
 		
@@ -73,42 +69,7 @@ namespace EA_ReqIF_AddIn
 					break;
 
 				default:
-					throw new ParserFailureException(unexpectedElementNodeError + name + "'.");
-			}
-		}
-		
-		public override void ProcessAttribute(string name, string attribValue)
-		{
-			switch (name)
-			{
-				case identifierAttributeName:
-					identifiableElementUnderConstruction.Identifier = attribValue;
-					break;
-					
-				case descriptionAttributeName:
-					identifiableElementUnderConstruction.Description = attribValue;
-					break;
-					
-				case longNameAttributeName:
-					identifiableElementUnderConstruction.LongName = attribValue;
-					break;
-					
-				case lastChangeAttributeName:
-					identifiableElementUnderConstruction.LastChange = ConvertStringifiedDateTime(attribValue);
-					break;
-					
-				default:
-					throw new ParserFailureException(unexpectedAttributeError + name + "'.");
-			}
-		}
-		
-		private void FinalizeIdentifiableElementUnderConstruction()
-		{
-			if (identifiableElementUnderConstruction != null &&
-			   identifiableElementUnderConstruction.Identifier != string.Empty)
-			{
-				specificationTypes.Add(identifiableElementUnderConstruction.Identifier,
-				                       identifiableElementUnderConstruction);
+					throw new ParserFailureException(unexpectedElementNodeErrorText + name + "'.");
 			}
 		}
 	}
