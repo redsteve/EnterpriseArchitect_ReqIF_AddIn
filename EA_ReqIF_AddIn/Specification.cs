f52eda606d5f1d96b5c5720f450623d44778da57
redsteve <redsteve@gmx.de>
2011-07-05 22:04:23 +0200

    New class: Specification.

---
+++ EA_ReqIF_AddIn/Specification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EA_ReqIF_AddIn
+{
+	/// <summary>
+	/// This class represents an ReqIF element <c>&lt;SPECIFICATION&gt;</c>...<c>&lt;/SPECIFICATION&gt;</c>.
+	/// See OMG ReqIF specification chapter 10.8.37 for more information.
+	/// </summary>
+	public class Specification : Identifiable
+	{
+		private string alternativeId;
+		private string specificationTypeIdentifier;
+		
+		public string AlternativeId {
+			get { return alternativeId; }
+			set { alternativeId = value; }
+		}
+		
+		public string SpecificationTypeIdentifier {
+			get { return specificationTypeIdentifier; }
+			set { specificationTypeIdentifier = value; }
+		}
+		
+		public Specification()
+		{
+		}
+	}
+}
