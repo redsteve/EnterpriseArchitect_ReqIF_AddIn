700bfc4d4d70887ef49392a629513cd886f9d922
redsteve <redsteve@gmx.de>
2011-07-12 06:21:50 +0200

    New exception class added.

---
+++ EA_ReqIF_AddIn/EnterpriseArchitectInteropFailure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EA_ReqIF_AddIn
+{
+	/// <summary>
+	/// The exception that is thrown when an error occurs while Enterprise Architect API.
+	/// </summary>
+	public class EnterpriseArchitectInteropFailure : ApplicationException
+	{
+		public EnterpriseArchitectInteropFailure() : base("An error has occured while parsing the file!")
+		{
+		}
+		
+		public EnterpriseArchitectInteropFailure(string message) : base(message)
+		{
+		}
+		
+		public EnterpriseArchitectInteropFailure(SerializationInfo info, StreamingContext context) :
+			base(info, context)
+		{
+		}
+		
+		public EnterpriseArchitectInteropFailure(string message, Exception innerException) :
+			base(message, innerException)
+		{
+		}
+	}
+}
